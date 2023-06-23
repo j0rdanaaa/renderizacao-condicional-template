ff027b60fe702bffb98b2303fa4dc5cdf84f0c4d
j0rdanaaa <j0rdanaaa@gmail.com>
2023-06-23 16:41:14 -0300

    template redenrização condicional

--- src/components/TelaCadastro/TelaCadastro.js
+++ src/components/TelaCadastro/TelaCadastro.js
@@ -1,13 +1,17 @@
 import { Form, FormContainer, Input, StyledLabel, SendButton, BackToLoginButton } from "./styled";
 
-function TelaCadastro() {
+function TelaCadastro(props) {
+
+  //console.log("cad", props)
   const cadastrar = () => {
     // validação de cadastro ainda será visto mais pra frente no curso
-    alert("Cadastro realizado com sucesso, mas falta fazer a prática 3 e remover esse alerta")
+    //alert("Cadastro realizado com sucesso, mas falta fazer a prática 3 e remover esse alerta")
+    props.mudarTela("principal")
   }
 
   const mostrarTelaLogin = () => {
-    alert("Faça a prática 2 e remova esse alerta")
+    //alert("Faça a prática 2 e remova esse alerta")
+    props.mudarTela("login")
   }
 
   return (
--- src/components/TelaPrincipal/TelaPrincipal.js
+++ src/components/TelaPrincipal/TelaPrincipal.js
@@ -1,9 +1,10 @@
 import { Div, Titulo } from "./styled";
 
-function TelaPrincipal() {
+function TelaPrincipal(props) {
   const deslogar = () => {
     // validação de logout ainda será visto mais pra frente no curso
-    alert("Sessão encerrada com sucesso, mas falta fazer a prática 3 e remover esse alerta")
+    //alert("Sessão encerrada com sucesso, mas falta fazer a prática 3 e remover esse alerta")
+    props.mudarTela("login")
   }
 
   return (
